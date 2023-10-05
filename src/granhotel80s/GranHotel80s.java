3b5c4caf837e28fb055c71b9d0e917f687539a7f
orli1208 <116685619+orli1208@users.noreply.github.com>
2023-10-05 20:59:20 -0300

    Añadi HabitacionData

--- src/granhotel80s/GranHotel80s.java
+++ src/granhotel80s/GranHotel80s.java
@@ -6,7 +6,9 @@
 package granhotel80s;
 
 import granhotel80s.accesoADatos.Conexion;
+import granhotel80s.accesoADatos.HabitacionData;
 import granhotel80s.accesoADatos.HuespedData;
+import granhotel80s.entidades.Habitacion;
 import granhotel80s.entidades.Huesped;
 import java.sql.Connection;
 
@@ -22,25 +24,40 @@ public class GranHotel80s {
     public static void main(String[] args) {
         
         Connection con = Conexion.getConnection();
-        
         HuespedData hd = new HuespedData();
+        HabitacionData habiData = new HabitacionData();
+        
         //agregar Huesped
-//       Huesped persona = new Huesped(2, 42862742, "Or", "Fornas", "orlandofornas@gmail.com", "Reconquista", "2615970271", true);
+        
+        //Huesped persona = new Huesped(2, 42862742, "Or", "Fornas", "orlandofornas@gmail.com", "Reconquista", "2615970271", true);
         //hd.guardarHuesped(persona);
         
-         //Eliminar Huesped 
+//----------------------------------------------------------------------------------------------------      
+
+        //Eliminar Huesped 
         //hd.eliminarHuesped(2);
         
-        // buscar huesped
-//        hd.BuscarHuesped(2);
+//----------------------------------------------------------------------------------------------------
+        
+        //buscar huesped
+        //hd.BuscarHuesped(2);
 
-          // Modificar Datos del huesped
-//            hd.modificarHuesped(persona);
-    
-    
+//----------------------------------------------------------------------------------------------------
 
+          //Modificar Datos del huesped
+          //hd.modificarHuesped(persona);
+    
+//----------------------------------------------------------------------------------------------------
+
+        //Agragar habitacion
+        Habitacion habi = new Habitacion(1, "Simple", 2, 1, true);
+        //habiData.guardarHabitacion(habi);
           
-       
+//----------------------------------------------------------------------------------------------------
+
+        //Modificar habitacion
+        habiData.modificarHabitacion(habi);
+        
     }
 
     
